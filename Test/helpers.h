c7fdb74cced85327d7f33f40297052fd6c15aad7
Teodor Dutu <teodor.dutu@gmail.com>
2019-05-17 13:42:54 +0300

    Test laborator

---
+++ Test/helpers.h
@@ -0,0 +1,28 @@
+#ifndef _HELPERS_H
+#define _HELPERS_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 13
+
+/*
+ * Macro de verificare a erorilor
+ * Exemplu:
+ * 		int fd = open (file_name , O_RDONLY);
+ * 		DIE( fd == -1, "open failed");
+ */
+
+#define DIE(assertion, call_description)				\
+	do {								\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",			\
+					__FILE__, __LINE__);		\
+			perror(call_description);			\
+			exit(EXIT_FAILURE);				\
+		}							\
+	} while(0)
+
+/* Dimensiunea maxima a calupului de date */
+#define BUFLEN 1500
+
+#endif
